8dca85d0403bb15122c17c95f71912cdfc0b0674
intertext.no <orstavik77@gmail.com>
2022-01-13 10:34:03 +0100

    medium step simplification

--- test/constructionFrame.js
+++ test/constructionFrame.js
@@ -7,21 +7,21 @@
     #parent;
     #state = "start";
     #elements;
+    #name;
 
-    static #observers = {'start': [], 'end': [], 'complete': []};
+    static #observers = {'end': [], 'complete': []};
 
-    constructor() {
+    constructor(name) {
       this.#parent = now;
+      this.#name = name || this.constructor.name.slice(0, -17);
       now = this;
       this.#parent?.#children.push(this);
-      // this.#callObservers('start');
     }
 
     #callObservers(state) {
       const observers = ConstructionFrame.#observers[this.#state = state];
-      for (let cb of observers)
-        // cb(this);
-        for (let el of this.elements())
+      for (let el of this.elements())
+        for (let cb of observers)
           cb(this, el);
     }
 
@@ -44,7 +44,7 @@
 
     toString() {
       const parent = this.#parent ? this.#parent.toString() + ', ' : '';
-      return parent + this.constructor.name.slice(0, -17) + '#' + this.#state;
+      return parent + this.#name + '#' + this.#state;
     }
 
     static get now() {
@@ -64,8 +64,8 @@
       pos >= 0 && this.#observers[state].splice(pos, 1);
     }
 
-    end() {
-      this.#elements = [...this.elementsImpl()];
+    end(elements = [...this.elementsImpl()]) {
+      this.#elements = elements;
       this.#callObservers('end');
       !this.#parent && this.#complete();
       now = this.#parent;
@@ -74,13 +74,6 @@
 
   window.ConstructionFrame = ConstructionFrame;
 
-  // function* recursiveNodes(n) {
-  //   yield n;
-  //   if (n.childNodes)
-  //     for (let c of n.childNodes)
-  //       yield* recursiveNodes(c);
-  // }
-  //
   function* recursiveElements(n) {
     yield n;
     if (n.children)
@@ -88,124 +81,124 @@
         yield* recursiveElements(c);
   }
 
-  class DocumentCreateElementConstructionFrame extends ConstructionFrame {
-    #el;
-
-    end(created) {
-      this.#el = created;
-      super.end();
-      return created;
-    }
-
-    // * nodes() {
+  // class DocumentCreateElementConstructionFrame extends ConstructionFrame {
+    // #el;
+    //
+    // end(created) {
+    //   this.#el = created;
+    //   super.end();
+      // return created;
+    // }
+    //
+    // * elementsImpl() {
     //   yield this.#el;
     // }
-    //
-    * elementsImpl() {
-      yield this.#el;
-    }
-  }
+  // }
 
-  class CloneNodeConstructionFrame extends ConstructionFrame {
-    #clone;
+  // class CloneNodeConstructionFrame extends ConstructionFrame {
+    // #clone;
 
-    end(clone) {
-      this.#clone = clone;
-      super.end();
-      return clone;
-    }
-
-    // * nodes() {
-    //   yield* recursiveNodes(this.#clone);
+    // end(clone) {
+      // this.#clone = clone;
+      // super.end([...recursiveElements(clone)]);
+      // return clone;
     // }
-    //
-    * elementsImpl() {
-      yield* recursiveElements(this.#clone);
-    }
-  }
 
-  class InnerHTMLConstructionFrame extends ConstructionFrame {
-    #el;
-
-    end(el) {
-      this.#el = el;
-      super.end();
-    }
-
-    // * nodes() {
-    //   for (let n of this.#el.childNodes)
-    //     yield* recursiveNodes(n);
+    // * elementsImpl() {
+    //   yield* recursiveElements(this.#clone);
     // }
-    //
-    * elementsImpl() {
-      for (let n of this.#el.children)
-        yield* recursiveElements(n);
-    }
+  // }
+
+  function* recursiveOfChildren(el) {
+    for (let n of el.children)
+      yield* recursiveElements(n);
   }
+
+  // class InnerHTMLConstructionFrame extends ConstructionFrame {
+    // #el;
+
+    // end(el) {
+      // this.#el = el;
+      // super.end([...recursiveOfChildren(el)]);
+    // }
+
+    //
+    // * elementsImpl(el) {
+    //   for (let n of this.#el.children)
+    //     yield* recursiveElements(n);
+    // }
+  // }
 
   function innerHTML_constructionFrame(og, val) {
-    const frame = new InnerHTMLConstructionFrame();
+    const frame = new ConstructionFrame("InnerHTML");
     og.call(this, val);
-    frame.end(this);
+    frame.end([...recursiveOfChildren(this)]);
   }
 
   MonkeyPatch.monkeyPatchSetter(Element.prototype, "innerHTML", innerHTML_constructionFrame);
   MonkeyPatch.monkeyPatchSetter(ShadowRoot.prototype, "innerHTML", innerHTML_constructionFrame);
 
   MonkeyPatch.monkeyPatch(Node.prototype, "cloneNode", function cloneNode_constructionFrame(og, ...args) {
-    return new CloneNodeConstructionFrame().end(og.call(this, ...args));
+    const frame = new ConstructionFrame("CloneNode");
+    const clone = og.call(this, ...args);
+    frame.end([...recursiveElements(clone)]);
+    return clone;
   });
 
   MonkeyPatch.monkeyPatch(Document.prototype, "createElement", function createElement_constructionFrame(og, ...args) {
-    return new DocumentCreateElementConstructionFrame().end(og.call(this, ...args));
+    const frame = new ConstructionFrame("DocumentCreateElement");
+    const created = og.call(this, ...args);
+    frame.end([created]);
+    return created;
   });
 
-  class InsertAdjacentHTMLConstructionFrame extends ConstructionFrame {
-    #d;
-
-    end({position, element, previousSibling, lastChild, firstChild, nextSibling}) {
-      this.#d = {position, element, previousSibling, lastChild, firstChild, nextSibling};
-      super.end();
-    }
-
-    // * nodes() {
-    //   if (this.#d.position === 'beforebegin') {
-    //     for (let n = this.#d.previousSibling?.nextSibling || this.#d.element.parentNode?.firstChild; n && n !== this.#d.element; n = n.nextSibling)
-    //       yield* recursiveNodes(n);
-    //   } else if (this.#d.position === 'afterend') {
-    //     for (let n = this.#d.element.nextSibling; n && n !== this.#d.nextSibling; n = n.nextSibling)
-    //       yield* recursiveNodes(n);
-    //   } else if (this.#d.position === 'afterbegin') {
-    //     for (let n = this.#d.element.firstChild; n && n !== this.#d.firstChild; n = n.nextSibling)
-    //       yield* recursiveNodes(n);
-    //   } else if (this.#d.position === 'beforeend') {
-    //     for (let n = this.#d.lastChild?.nextSibling || this.#d.element.firstChild; n; n = n.nextSibling)
-    //       yield* recursiveNodes(n);
-    //   }
-    // }
-    //
-    * elementsImpl() {
-      if (this.#d.position === 'beforebegin') {
-        for (let n = this.#d.previousSibling?.nextSibling || this.#d.element.parentNode?.firstChild; n && n !== this.#d.element; n = n.nextSibling)
-          if (n instanceof Element) yield* recursiveElements(n);
-      } else if (this.#d.position === 'afterend') {
-        for (let n = this.#d.element.nextSibling; n && n !== this.#d.nextSibling; n = n.nextSibling)
-          if (n instanceof Element) yield* recursiveElements(n);
-      } else if (this.#d.position === 'afterbegin') {
-        for (let n = this.#d.element.firstChild; n && n !== this.#d.firstChild; n = n.nextSibling)
-          if (n instanceof Element) yield* recursiveElements(n);
-      } else if (this.#d.position === 'beforeend') {
-        for (let n = this.#d.lastChild?.nextSibling || this.#d.element.firstChild; n; n = n.nextSibling)
-          if (n instanceof Element) yield* recursiveElements(n);
-      }
+function * elementsImplx(d /*= this.#d*/) {
+    if (d.position === 'beforebegin') {
+      for (let n = d.previousSibling?.nextSibling || d.element.parentNode?.firstChild; n && n !== d.element; n = n.nextSibling)
+        if (n instanceof Element) yield* recursiveElements(n);
+    } else if (d.position === 'afterend') {
+      for (let n = d.element.nextSibling; n && n !== d.nextSibling; n = n.nextSibling)
+        if (n instanceof Element) yield* recursiveElements(n);
+    } else if (d.position === 'afterbegin') {
+      for (let n = d.element.firstChild; n && n !== d.firstChild; n = n.nextSibling)
+        if (n instanceof Element) yield* recursiveElements(n);
+    } else if (d.position === 'beforeend') {
+      for (let n = d.lastChild?.nextSibling || d.element.firstChild; n; n = n.nextSibling)
+        if (n instanceof Element) yield* recursiveElements(n);
     }
   }
 
+
+  // class InsertAdjacentHTMLConstructionFrame extends ConstructionFrame {
+    // #d;
+
+    // end({position, element, previousSibling, lastChild, firstChild, nextSibling}) {
+      // this.#d = ;
+      // super.end([...elementsImplx({position, element, previousSibling, lastChild, firstChild, nextSibling})]);
+    // }
+
+    // * elementsImpl(d /*= this.#d*/) {
+    //   if (d.position === 'beforebegin') {
+    //     for (let n = d.previousSibling?.nextSibling || d.element.parentNode?.firstChild; n && n !== d.element; n = n.nextSibling)
+    //       if (n instanceof Element) yield* recursiveElements(n);
+    //   } else if (d.position === 'afterend') {
+    //     for (let n = d.element.nextSibling; n && n !== d.nextSibling; n = n.nextSibling)
+    //       if (n instanceof Element) yield* recursiveElements(n);
+    //   } else if (d.position === 'afterbegin') {
+    //     for (let n = d.element.firstChild; n && n !== d.firstChild; n = n.nextSibling)
+    //       if (n instanceof Element) yield* recursiveElements(n);
+    //   } else if (d.position === 'beforeend') {
+    //     for (let n = d.lastChild?.nextSibling || d.element.firstChild; n; n = n.nextSibling)
+    //       if (n instanceof Element) yield* recursiveElements(n);
+    //   }
+    // }
+  // }
+
   MonkeyPatch.monkeyPatch(Element.prototype, "insertAdjacentHTML", function insertAdjacentHTML_constructHtmlElement(og, position, ...args) {
     const {previousSibling, lastChild, firstChild, nextSibling} = this;
-    const frame = new InsertAdjacentHTMLConstructionFrame();
+    const frame = new ConstructionFrame("InsertAdjacentHTML");
     og.call(this, position, ...args);
-    frame.end({position, element: this, previousSibling, lastChild, firstChild, nextSibling});
+    frame.end([...elementsImplx({position, element: this, previousSibling, lastChild, firstChild, nextSibling})]);
   });
 })();
 /*
@@ -228,10 +221,6 @@
       new UpgradeConstructionFrame(this.#tagName, el);   //and start a new frame
     }
 
-    // * nodes() {
-    //   if (this.#el) yield this.#el;
-    // }
-    //
     * elementsImpl() {
       if (this.#el) yield this.#el;
     }
@@ -258,19 +247,16 @@
   if (document.readyState !== 'loading')
     return;
 
-  // function* recursiveNodesWithSkips(el, skips) {
-  //   yield el;
-  //   if (el.childNodes)
-  //     for (let c of el.childNodes)
-  //       if (skips.indexOf(c) < 0)
-  //         yield* recursiveNodesWithSkips(c, skips);
-  // }
-  //
   function* recursiveElementsWithSkips(el, skips) {
     yield el;
     for (let c of el.children)
       if (skips.indexOf(c) < 0)
         yield* recursiveElementsWithSkips(c, skips);
+  }
+
+  function * elementsImplY(el, skips) {
+    for (let n of recursiveElementsWithSkips(/*this.#*/el, /*this.#*/skips))
+      yield n;
   }
 
   class PredictiveConstructionFrame extends ConstructionFrame {
@@ -284,45 +270,31 @@
     }
 
     end(skips) {
-      if(!this.#el.isConnected) return;   //if the constructor fails, then the PredictiveConstructionFrame should also fail as such.
+      if (!this.#el.isConnected) return;   //if the constructor fails, then the PredictiveConstructionFrame should also fail as such.
       this.#skips = skips;
-      super.end();
+      super.end([...elementsImplY(this.#el, skips)]);
     }
 
     get el() {
       return this.#el;
     }
 
-    // * nodes() {
-    //   for (let n of recursiveNodesWithSkips(this.#el, this.#skips))
-    //     yield n;
-    // }
-    //
-    * elementsImpl() {
-      for (let n of recursiveElementsWithSkips(this.#el, this.#skips))
+  }
+  function * elementsImplZ(nodes) {
+    for (let n of /*this.#*/nodes)
+      if (n instanceof Element)
         yield n;
-    }
   }
 
-  class ParserConstructionFrame extends ConstructionFrame {
-    #nodes;
+  // class ParserConstructionFrame extends ConstructionFrame {
+    // #nodes;
 
-    end(nodes) {
-      this.#nodes = nodes;
-      super.end();
-    }
-
-    // * nodes() {
-    //   for (let n of this.#nodes)
-    //     yield n;
+    // end(nodes) {
+      // this.#nodes = nodes;
+      // super.end([...elementsImplZ(nodes)]);
     // }
-    //
-    * elementsImpl() {
-      for (let n of this.#nodes)
-        if (n instanceof Element)
-          yield n;
-    }
-  }
+
+  // }
 
   const roots = [];
   const frames = [];
@@ -340,7 +312,7 @@
     //todo Now we calculate the full list of nodes for the new FarserFrame. We only need to calculate that it has one, and then calculate the rest when queried.
     //todo so this might be made slightly more efficient.
     const addedNodes = [...e.endedNodes()].filter(n => roots.findIndex(s => s === n || s.compareDocumentPosition(n) & Node.DOCUMENT_POSITION_CONTAINED_BY) === -1);
-    addedNodes.length && new ParserConstructionFrame().end(addedNodes);
+    addedNodes.length && new ConstructionFrame("Parser").end([...elementsImplZ(addedNodes)]);
     // todo here we can remove the elements that has been ended from the roots list. Not sure if this would be faster though.
     // roots.splice(roots.indexOf(frame.el), 1); //todo this doesn't work. the roots are popping up later.
   }
